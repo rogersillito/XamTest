0470b62e689bb46895f2b81304321451578c0c31
Roger Sillito <rsillito@scottlogic.co.uk>
2019-02-19 17:07:04 +0000

    resource vs local loaded images.  Resource loading markup helper.  Experimenting with grid layout.

--- XamTest/XamTest/AddSomething.xaml.cs
+++ XamTest/XamTest/AddSomething.xaml.cs
@@ -1,19 +1,32 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamTest.ViewModels;
+using System.Reflection;
 
 namespace XamTest
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddSomething : ContentPage
     {
-        public AddSomething(MainPageViewModel mainPageViewModel)
+        public AddSomething()
         {
             InitializeComponent();
+
+            //sunflower.Source = ImageSource.FromResource("XamTest.Assets.sunflower.png");
+        }
+
+        public AddSomething(MainPageViewModel mainPageViewModel): this()
+        {
             BindingContext = mainPageViewModel;
 
             var imageSource = ImageSource.FromResource("XamTest.Assets.vegetables.jpg");
             NavigationPage.SetTitleView(this, MainPage.CreateImageTitleView(imageSource));
+
+            var assembly = GetType().GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            }
         }
     }
 }
