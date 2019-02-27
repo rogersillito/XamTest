8bd13ac890ee04d6829644a97d7f194d390907f8
Roger Sillito <rsillito@scottlogic.co.uk>
2019-02-27 17:56:10 +0000

    add project to do selective generation of openauth api client/controller

---
+++ ApiGenerator/Program - Copy.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+////Install-Package CommandLineParser
+//using CommandLine;
+
+//namespace ApiGenerator
+//{
+//    [Verb("controller", HelpText = "Generate C# Controllers")]
+//    public class ControllerOptions
+//    {
+
+//    }
+
+//    [Verb("client", HelpText = "Generate C# Clients")]
+//    public class ClientOptions
+//    {
+
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Parser.Default.ParseArguments<ClientOptions, ControllerOptions>(args)
+//                .WithParsed<ClientOptions>(opt =>
+//                {
+//                    //TODO: client gen
+
+//                })
+//                .WithParsed<ControllerOptions>(opt =>
+//                {
+//                    //TODO: controller gen
+
+//                });
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
---
+++ ApiGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+namespace ApiGenerator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://raw.githubusercontent.com/OpenBankingUK/read-write-api-specs/v3.1.0/dist/account-info-swagger.json";
+            var document = SwaggerDocument.FromUrlAsync(url).Result;
+
+            var requiredPathItemKeys = new List<string>
+            {
+                "/accounts",
+                "/accounts/{AccountId}",
+                "/accounts/{AccountId}/balances",
+                "/accounts/{AccountId}/transactions",
+                "/accounts/{AccountId}/statements",
+                "/accounts/{AccountId}/statements/{StatementId}"
+            };
+
+            var pathItemKeys = document.Paths.Keys.OrderBy(k => k).ToList();
+            for (var i = pathItemKeys.Count - 1; i >= 0; i--)
+            {
+                var pathKey = pathItemKeys[i];
+                if (!requiredPathItemKeys.Contains(pathKey))
+                {
+                    document.Paths.Remove(pathKey);
+                }
+            }
+
+
+        }
+    }
+}
