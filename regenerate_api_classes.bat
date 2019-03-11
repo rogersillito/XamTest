f6392443b3d4c03779d4471b88a77ecb24deb1e2
Roger Sillito <rogersillito@gmail.com>
2019-03-11 11:22:19 +0000

    restore .NET framework api

--- regenerate_api_classes.bat
+++ regenerate_api_classes.bat
@@ -1,2 +1,2 @@
-"%~dp0ApiGenerator\bin\debug\ApiGenerator" run "%~dp0OpenBankingApi\OpenBankingApi_v3.1.0.nswag"
+ApiGenerator\bin\debug\ApiGenerator run OpenBankingApi\OpenBankingApi_v3.1.0.nswag
 @ping 127.0.0.1 -n 3 > nul
